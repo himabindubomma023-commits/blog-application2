290c47553fc7950e59f538aad022690be14fe8ae
Himabindu Bomma <himabindubomma023@gmail.com>
2026-09-20 17:31:25 +0530

    Prepare application for deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const JWT_SECRET =
     process.env.JWT_SECRET || "myblog_secret_2026";
